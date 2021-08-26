8b4628b2e5db3fe9c19c015febedb513714b10a1
sudhaparayil <parayilsudha@gmail.com>
2021-08-26 13:32:49 +0530

    customize register page done

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -24,13 +24,18 @@ class CreateNewUser implements CreatesNewUsers
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
+            'phone' => ['required','regex:/^([0-9\s\-\+\(\)]*)$/','min:10'],
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['required', 'accepted'] : '',
         ])->validate();
+
+        $registeras = $input['registeras'] === 'SVP' ? 'SVP' : 'CST'; 
 
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'phone' => $input['phone'],
+            'usertype' => $registeras
         ]);
     }
 }
